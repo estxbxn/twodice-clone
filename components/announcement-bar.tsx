ef908db248e07ebd031059e63d30efe5b31fc26c
Estéban Piecourt <esteban.piecourt@gmail.com>
2025-08-10 01:10:45 +0200

    refactor: uses button component for dismiss icon

--- components/announcement-bar.tsx
+++ components/announcement-bar.tsx
@@ -2,6 +2,7 @@
 
 import { XIcon } from "lucide-react";
 import { useState } from "react";
+import { Button } from "./ui/button";
 
 export function AnnouncementBar() {
   const [dismissed, setDismissed] = useState(false);
@@ -9,15 +10,18 @@ export function AnnouncementBar() {
   return (
     <div className="mt-4 w-full">
       <div className="rounded-xl md:rounded-2xl bg-[linear-gradient(90deg,theme(colors.fuchsia.500),theme(colors.indigo.400),theme(colors.cyan.400),white,theme(colors.cyan.400),theme(colors.indigo.400),theme(colors.fuchsia.500))]">
-        <div className="text-xs md:text-sm text-black/90 dark:text-black px-3 md:px-4 py-4 flex items-center justify-between gap-3 bg-white/70 rounded-[10px] md:rounded-[14px] backdrop-blur">
+        <div className="text-xs md:text-sm text-black/90 px-3 md:px-4 py-4 flex items-center justify-between gap-3 bg-white/70 rounded-[10px] md:rounded-[14px] backdrop-blur">
           <p className="font-semibold">
             We will be at GG Gamers Asia 2024! We look forward to seeing you in 2024 from 17 - 20 October 2024 at Singapore at Marina Convention Centre.
           </p>
-          <button
+          <Button
+            variant="link"
+            size="icon"
+            className="w-auto h-auto text-black/90"
             onClick={() => setDismissed(true)}
           >
             <XIcon className="size-4" />
-          </button>
+          </Button>
         </div>
       </div>
     </div>
