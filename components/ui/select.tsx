668b3dc3a6ea131dd34a172c529812e866b670cf
Estéban Piecourt <esteban.piecourt@gmail.com>
2025-08-10 01:10:34 +0200

    feat: adds cursor pointer to button/select

--- components/ui/select.tsx
+++ components/ui/select.tsx
@@ -63,7 +63,7 @@ function SelectTrigger({ className, children, size = "default" }: React.Componen
       data-slot="select-trigger"
       data-size={size}
       className={cn(
-        "border-input data-[placeholder]:text-muted-foreground focus-visible:border-ring focus-visible:ring-ring/50 dark:bg-input/30 dark:hover:bg-input/50 flex items-center gap-2 rounded-md border border-b-3 bg-background px-3 py-2 text-sm whitespace-nowrap shadow-xs outline-none focus-visible:ring-[3px] disabled:cursor-not-allowed disabled:opacity-50",
+        "cursor-pointer border-input data-[placeholder]:text-muted-foreground focus-visible:border-ring focus-visible:ring-ring/50 dark:bg-input/30 dark:hover:bg-input/50 flex items-center gap-2 rounded-md border border-b-3 bg-background px-3 py-2 text-sm whitespace-nowrap shadow-xs outline-none focus-visible:ring-[3px] disabled:cursor-not-allowed disabled:opacity-50",
         size === "default" ? "h-9" : "h-8",
         className
       )}
